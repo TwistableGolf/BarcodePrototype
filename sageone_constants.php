04d56e9431b4150fcb7a23ccd44607848d7fd796
TwistableGolf <lewissavage88@gmail.com>
2018-01-17 23:26:36 +0000

    Requests Added

---
+++ sageone_constants.php
@@ -0,0 +1,17 @@
+<?php
+  $client_id = '10e38a2c72916561060a';
+  $client_secret = 'f476a3744d3546b854af569124246b9c2d0ad29e';
+  $signing_secret = 'd48c9039eb903951f2fbd844ecc1a2796c5ab40f';
+  $apim_subscription_key = '4a776833cf5f4582bcf7587af30d4dd1';
+  $callback_url = 'http://localhost/test/callback.php';
+  $scope = 'full_access';
+  $auth_endpoint = 'http://www.sageone.com/oauth2/auth';
+  $us_token_endpoint = 'http://mysageone.na.sageone.com/oauth2/token';
+  $ca_token_endpoint = 'http://mysageone.ca.sageone.com/oauth2/token';
+  $token_endpoint = 'http://app.sageone.com/oauth2/token';
+  $uki_token_endpoint = 'http://app.sageone.com/oauth2/token';
+  $us_base_endpoint = 'https://api.columbus.sage.com/us/sageone/';
+  $ca_base_endpoint = 'https://api.columbus.sage.com/ca/sageone/';
+  $uki_base_endpoint = 'https://api.columbus.sage.com/uki/sageone/';
+
+?>
